45fa222e467a0d77652fd186290f61ebf08317df
Jun Nishimura <n.junjun0303@gmail.com>
2024-06-01 15:17:19 +0900

    add package manager

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+;; package-initialization
+(package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+	("melpa" . "http://melpa.org/packages/")
+	("org" . "http://orgmode.org/elpa/")))
+
 ;; make emacs fullscreen when to open
 (set-frame-parameter nil 'fullscreen 'maximized)
 
@@ -5,3 +12,28 @@
 (if window-system
     (progn
       (set-frame-parameter nil 'alpha 95)))
+
+;; straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(setq package-enable-at-startup nil)
+(straight-use-package 'use-package)
+
+;; auto-save
+(use-package auto-save-buffers-enhanced
+  :config
+  (setq auto-save-buffers-enhanced-interval 10)
+  (auto-save-buffers-enhanced t))
