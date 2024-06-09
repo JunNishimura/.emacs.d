db7727b60eab028c46f8cae0c569356d9637cc6a
Jun Nishimura <n.junjun0303@gmail.com>
2024-06-10 01:13:45 +0900

    change warning level

--- init.el
+++ init.el
@@ -191,20 +191,4 @@
   (setq iflipb-wrap-around t))
 
 ;; warning
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(warning-suppress-log-types
-   '(((copilot copilot-no-mode-indent))
-     (copilot copilot-exceeds-max-char)))
- '(warning-suppress-types
-   '(((copilot copilot-no-mode-indent))
-     ((copilot copilot-no-mode-indent)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq warning-minimum-level :error)
